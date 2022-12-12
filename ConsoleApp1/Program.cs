87dc4fdca3d104f7701c45bd9aa6cff931d49ec0
lfons <lfons@DESKTOP-8PJVQIC>
2022-12-12 17:40:12 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Inicio
+
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+class MetodosEstaticos
+{
+    public static int somar (int x, int y)
+    {
+        return x + y;
+    }
+    public int multiplicar (int x, int y)
+    {
+        return x * y;
+    }
+
+    static void Main(string[] args)
+    {
+        var result = MetodosEstaticos.somar(3, 10);
+        Console.WriteLine("A soma é igual a: {0}", result);
+
+        MetodosEstaticos calc = new MetodosEstaticos();
+        Console.WriteLine(calc.multiplicar(3, 40));
+    }
+    
+}
